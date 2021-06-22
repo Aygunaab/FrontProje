8e0d5235b0026eae90bed344c7687003a856b143
unknown <aygunaab@code.edu.az>
2021-06-22 03:24:52 -0700

    History

--- TransX Front/assets/js/main.js
+++ TransX Front/assets/js/main.js
@@ -1,7 +1,4 @@
 
-
-
-
 $(document).ready(function() {
 	$('.count-num').counterUp({
 		delay: 10,
@@ -102,6 +99,38 @@ $('.logo-slider').slick({
 				slidesToShow: 1,
 				slidesToScroll: 1,
 				dots: true
+			}
+		}
+	]
+});
+$('.about-slider').slick({
+	slidesToShow: 3,
+	slidesToScroll: 2,
+	dots: true,
+	arrows: false,
+	appendDots: $('.about-slider-nav'),
+	responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 2,
+				infinite: true,
+				dots: true
+			}
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 4
 			}
 		}
 	]
